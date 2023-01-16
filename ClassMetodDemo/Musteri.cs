d5b9801e10b53ea3ad98b1f4b4fca2e7cfeb4e2c
Furkannkrts <furkann.krts@hotmail.com>
2023-01-16 16:41:10 +0300

    bug fix

--- ClassMetodDemo/Musteri.cs
+++ ClassMetodDemo/Musteri.cs
@@ -12,9 +12,7 @@ namespace ClassMetodDemo
         public string Name { get; set; }
         public string Surname { get; set; }
         public DateTime BirthDate { get; set; }
-        public DateTime DeathDate { get; set; }
-
-
+    
         
     }
 }
