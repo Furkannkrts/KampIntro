771366f51597f927a9f96c7fec9b683c4aa88c99
Furkannkrts <furkann.krts@hotmail.com>
2023-01-17 10:46:01 +0300

    Revert "Delete Mathematics directory"
    This reverts commit 41762684fa883d0ec7bec4705e476b0963166a86.

---
+++ Mathematics/DortIslem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    internal class DortIslem
+    {
+        public void Topla(int sayi1,int sayi2)
+        {
+            int toplam=sayi1+ sayi2;    
+            Console.WriteLine("sonuç:" + toplam); 
+
+        }
+        public void Carp(int sayi1,int sayi2) 
+        {
+            int carp=sayi1* sayi2;
+            Console.WriteLine("sonuc:"+ carp);
+        
+        }
+        public void Bolme(int sayi1,int sayi2)
+        {
+            int bolme= sayi1/sayi2;
+            Console.WriteLine("sonuc:" + bolme);
+        }
+    }
+}
